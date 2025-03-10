eeafc08030e50754030a73c74e4cb983c5fcfac4
Neil Mayhew <neil@mayhew.name>
2025-03-10 14:49:16 -0600

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A library for accessing the /proc filesystem in Linux";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      name = "system-linux-proc";
+      compiler = "ghc910";
+    in
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs name;
+      overlay = final: prev: {
+        "${name}".defaultPackage = final.haskell.packages.${compiler}.callPackage ./default.nix {};
+      };
+      shell = { pkgs }: pkgs.${name}.defaultPackage.env;
+    };
+}
